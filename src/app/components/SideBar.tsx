0f95b3bee63936f1b88a5becb7fd220ade4fcf96
guido morabito <guidomorabito161@hotmail.com>
2024-05-26 23:25:22 -0300

    poke app

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -1,18 +1,19 @@
 import Image from 'next/image'
 import React from 'react'
-import { IoBrowsersOutline, IoCalculator, IoLogoReact } from 'react-icons/io5'
+import { IoBrowsersOutline, IoCalculator, IoFootball, IoLogoReact } from 'react-icons/io5'
 import { SideBarMenuItem } from './SideBarMenuItem'
 
 const menuItems = [
     {path:'/dashboard/main', icon:<IoBrowsersOutline size={40}/>, title:'Dashboard', subTitle:'Visualizacion'},
-    {path:'/dashboard/counter', icon:<IoCalculator size={40}/>, title:'Counter', subTitle:'Contador Client Side'}
+    {path:'/dashboard/counter', icon:<IoCalculator size={40}/>, title:'Counter', subTitle:'Contador Client Side'},
+    {path:'/dashboard/pokemons', icon:<IoFootball size={40}/>, title:'Pokemons', subTitle:'Generacion estatica'}
 ]
 
 export const SideBar = () => {
     return (
         <div
             style={{ width: "400px" }}
-            id="menu" className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen overflow-y-scroll">
+            id="menu" className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 overflow-y-scroll">
             <div id="logo" className="my-4 px-6">
                 <h1 className="flex items-center text-lg md:text-2xl font-bold text-white">
                     <IoLogoReact className="mr-2" />
